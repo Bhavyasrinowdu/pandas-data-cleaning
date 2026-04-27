3ee2fbeb132cd069f4b81fdf9a36be8a9e33acee
Bhavyasrinowdu <bhavyasrinowdu@gmail.com>
2026-04-27 14:01:00 +0530

    Add files via upload

---
+++ create_csv.py
@@ -0,0 +1,9 @@
+import pandas as pd
+data = {
+    "Name": ["Bhavya", "Anu", "Ravi"],
+    "Age": [20, 21, 22],
+    "City": ["Chennai", "Hyderabad", "Bangalore"]
+}
+df = pd.DataFrame(data)
+df.to_csv("data.csv", index=False)
+print("CSV created")
